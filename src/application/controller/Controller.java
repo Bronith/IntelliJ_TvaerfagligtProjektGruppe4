933edbc090215b443a0e3e9c4e635414e92611b5
dotdotcommaline <nikolaj9183@gmail.com>
2025-05-01 11:37:14 +0200

    test af git

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,4 +1,7 @@
 package application.controller;
 
+import application.models.produktion.Fad;
+
 public class Controller {
+    public Fad opretFad()
 }
--- src/application/models/produktion/Fad.java
+++ src/application/models/produktion/Fad.java
@@ -1,4 +1,22 @@
 package application.models.produktion;
 
+import application.models.produktion.decorator.Destillat;
+
 public class Fad {
+    private String navn;
+    private String nummer;
+    private String producent;
+    private String produktHistorik;
+    private String salgsHisorie;
+    private Destillat destillat;
+
+    public Fad(String navn, String nummer, String producent, String produktHistorik, String salgsHisorie) {
+        this.navn = navn;
+        this.nummer = nummer;
+        this.producent = producent;
+        this.produktHistorik = produktHistorik;
+        this.salgsHisorie = salgsHisorie;
+    }
+
+
 }
